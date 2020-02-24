fc23741d20a97232b4660c134fa0ea22d62ba78b
farzamnoori <farzamn@icloud.com>
2020-02-23 19:24:49 -0700

    updated

--- chat_room/settings.py
+++ chat_room/settings.py
@@ -85,7 +85,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('localhost', 6379)]
+            "hosts": [(os.environ.get('REDIS_URL', True), 13579)]
         }
     }
 }
